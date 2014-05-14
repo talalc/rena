dc0cf1bf13744a068c112344c503c137b0d9a4b2
talalc <talalchoudry@gmail.com>
2014-05-14 15:53:35 -0400

    p2 physics implemented

--- app/assets/javascripts/character.js
+++ app/assets/javascripts/character.js
@@ -2,16 +2,16 @@ var Character = Backbone.Model.extend({
   // Class constructor
   initialize: function (args){
     // Set the different geometries composing the humanoid
-    var head = new THREE.SphereGeometry(32, 8, 8),
-        hand = new THREE.SphereGeometry(8, 4, 4),
-        foot = new THREE.SphereGeometry(16, 4, 4, 0, Math.PI * 2, 0, Math.PI / 2),
-        nose = new THREE.SphereGeometry(4, 4, 4),
+    var head = new THREE.SphereGeometry(8, 8, 8),
+        hand = new THREE.SphereGeometry(2, 4, 4),
+        foot = new THREE.SphereGeometry(4, 4, 4, 0, Math.PI * 2, 0, Math.PI / 2),
+        nose = new THREE.SphereGeometry(1, 4, 4),
         // Set the material, the "skin"
         material = new THREE.MeshLambertMaterial(args);
     this.args = args;
     // Set the character modelisation object
     this.mesh = new THREE.Object3D();
-    this.mesh.position.y = 48;
+    this.mesh.position.y = 12;
     // Set and add its head
     this.head = new Physijs.SphereMesh(head, material);
     this.head.position.y = 0;
@@ -21,10 +21,10 @@ var Character = Backbone.Model.extend({
         left: new Physijs.SphereMesh(hand, material),
         right: new Physijs.SphereMesh(hand, material)
     };
-    this.hands.left.position.x = -40;
-    this.hands.left.position.y = -8;
-    this.hands.right.position.x = 40;
-    this.hands.right.position.y = -8;
+    this.hands.left.position.x = -10;
+    this.hands.left.position.y = -2;
+    this.hands.right.position.x = 10;
+    this.hands.right.position.y = -2;
     this.mesh.add(this.hands.left);
     this.mesh.add(this.hands.right);
     // Set and add its feet
@@ -32,18 +32,18 @@ var Character = Backbone.Model.extend({
         left: new Physijs.SphereMesh(foot, material),
         right: new Physijs.SphereMesh(foot, material)
     };
-    this.feet.left.position.x = -20;
-    this.feet.left.position.y = -48;
+    this.feet.left.position.x = -5;
+    this.feet.left.position.y = -12;
     this.feet.left.rotation.y = Math.PI / 4;
-    this.feet.right.position.x = 20;
-    this.feet.right.position.y = -48;
+    this.feet.right.position.x = 5;
+    this.feet.right.position.y = -12;
     this.feet.right.rotation.y = Math.PI / 4;
     this.mesh.add(this.feet.left);
     this.mesh.add(this.feet.right);
     // Set and add its nose
     this.nose = new Physijs.SphereMesh(nose, material);
     this.nose.position.y = 0;
-    this.nose.position.z = 32;
+    this.nose.position.z = 8;
     this.mesh.add(this.nose);
     // Set the vector of the current motion
     this.direction = new THREE.Vector3(0, 0, 0);
@@ -63,17 +63,21 @@ var Character = Backbone.Model.extend({
     this.physicMesh = new Physijs.SphereMesh(head, material);
     // Movement boolean
     this.canMove = true;
+    // Direction
+    this.direction = new THREE.Vector3(0,0,0);
   },
   control: function(){
     if ( Math.abs(this.pad.axes[0]) > 0.2 || Math.abs(this.pad.axes[1]) > 0.2 ){
       this.move(this.pad.axes[0],this.pad.axes[1]);
+    } else {
+      this.physicMesh.setLinearVelocity(new THREE.Vector3(0,-100,0));
     }
     if (this.pad.buttons[0] == 1 || this.pad.buttons[0].value == 1){
       this.jump();
-      var _this = this;
-      setTimeout(function(){
-        _this.fall();
-      }, 400);
+      // var _this = this;
+      // setTimeout(function(){
+      //   _this.fall();
+      // }, 400);
     }
     if (this.pad.buttons[2] == 1 || this.pad.buttons[2].value == 1){
       this.punch1();
@@ -85,8 +89,8 @@ var Character = Backbone.Model.extend({
   move: function(deltaX, deltaZ){
     if (this.canMove){
       this.walk();
-      this.mesh.position.x += deltaX;
-      this.mesh.position.z += deltaZ;
+      this.direction.set(deltaX*50,-100,deltaZ*50);
+      this.physicMesh.setLinearVelocity(this.direction);
       this.pointer.position.set(this.mesh.position.x + deltaX*15, this.mesh.position.y, this.mesh.position.z + deltaZ*15);
     }
   },
@@ -102,14 +106,16 @@ var Character = Backbone.Model.extend({
     }
   },
   jump: function(){
-    var _this = this;
-    var jumpinterval = setInterval(function() {
-      _this.mesh.position.y += 1;
-      // p1pointer.position.set(p1.mesh.position.x + pad1.axes[0]*15, p1.mesh.position.y, p1.mesh.position.z + pad1.axes[1]*15);
-    }, 100, this);
-    setTimeout( function(){
-      clearInterval(jumpinterval)
-    }, 400);
+    this.physicMesh.setAngularVelocity(new THREE.Vector3(0,0,0));
+    this.physicMesh.applyCentralImpulse(new THREE.Vector3(0,500000,0));
+    // var _this = this;
+    // var jumpinterval = setInterval(function() {
+    //   _this.mesh.position.y += 1;
+    //   // p1pointer.position.set(p1.mesh.position.x + pad1.axes[0]*15, p1.mesh.position.y, p1.mesh.position.z + pad1.axes[1]*15);
+    // }, 100, this);
+    // setTimeout( function(){
+    //   clearInterval(jumpinterval)
+    // }, 400);
   },
 
   fall: function(){
@@ -160,8 +166,8 @@ var Character = Backbone.Model.extend({
   step1: function(){
     var _this = this;
     var step1 = setInterval(function() {
-      _this.feet.left.position.z += 5;
-      _this.feet.right.position.z -= 5;
+      _this.feet.left.position.z += 2;
+      _this.feet.right.position.z -= 2;
     }, 100);
     setTimeout( function(){
       clearInterval(step1);
@@ -171,8 +177,8 @@ var Character = Backbone.Model.extend({
   step2: function(){
     var _this = this;
     var step2 = setInterval(function() {
-      _this.feet.left.position.z -= 5;
-      _this.feet.right.position.z += 5;
+      _this.feet.left.position.z -= 2;
+      _this.feet.right.position.z += 2;
     }, 100);
     setTimeout( function(){
       clearInterval(step2);
@@ -182,7 +188,7 @@ var Character = Backbone.Model.extend({
   punchanim1: function(){
     var _this = this;
     var step1 = setInterval(function() {
-      _this.hands.left.position.z += 7;
+      _this.hands.left.position.z += 1.5;
     }, 100);
     setTimeout( function(){
       clearInterval(step1);
@@ -192,7 +198,7 @@ var Character = Backbone.Model.extend({
   punchanim2: function(){
     var _this = this;
     var step2 = setInterval(function() {
-      _this.hands.left.position.z -= 7;
+      _this.hands.left.position.z -= 1.5;
     }, 100);
     setTimeout( function(){
       clearInterval(step2);
@@ -206,10 +212,10 @@ var Character = Backbone.Model.extend({
     }, 1000);
     this.hp -= 0.5;
     // this.mesh.position.set(this.mesh.position.x,this.mesh.position.y, this.mesh.position.z);
-    var b = new Physijs.SphereMesh( new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial( { color: 0xFF1111 } ));
-    b.position.set(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z);
-    bl.push(b);
-    scene.add(b);
+    // var b = new Physijs.SphereMesh( new THREE.SphereGeometry(1), new THREE.MeshBasicMaterial( { color: 0xFF1111 } ));
+    // b.position.set(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z);
+    // bl.push(b);
+    // scene.add(b);
   },
   hitCollisions: function(otherPlayer){
     var originPoint = this.mesh.position.clone();
